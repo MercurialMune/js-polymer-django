a0d148b97dbff0ace817ca78cadc59a94f9be40a
Cyprian Munene <45110252+MercurialMune@users.noreply.github.com>
2021-02-06 10:15:43 +0300

    Update settings.py

--- JSSortDjangoBackend/settings.py
+++ JSSortDjangoBackend/settings.py
@@ -47,8 +47,12 @@ WSGI_APPLICATION = 'JSSortDjangoBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sortjs',
+        'USER': 'cyprian',
+        'PASSWORD': 'cyprian',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
